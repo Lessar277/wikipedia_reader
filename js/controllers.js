a0e109b3a5669955be9009f9feae95322dc54b0a
Lessar27 <lessar277@gmail.com>
2016-06-08 19:51:54 +0300

    added search results; basic functionality done

--- js/controllers.js
+++ js/controllers.js
@@ -1,19 +1,39 @@
 angular.module('wikiReader')
     .controller('MainController', ['openSearch', function(openSearch) {
         var self = this;
+        this.input="";
 
-        self.getSuggestions = function(input, limit){
-            openSearch.suggest(input, limit, function(res){
+        this.getSuggestions = function(input, limit){
+            openSearch.getSuggestions(input, limit, function(res){
                 self.suggestions = res.data[1];
-                console.log(self.suggestions)
             })
         };
-        self.collapseHide = function() {
-            $('.collapse').collapse('hide');
+        this.collapseHide = function() {
+            setTimeout(function(){$('.collapse').collapse('hide')}, 100)
         }
-        self.collapser = function(input) {
+        this.collapser = function(input) {
             if (!input) {self.collapseHide()}
             else $('.collapse').collapse('show');
         }
+        this.setSearchValue = function(choice) {
+            self.input = choice;
+            console.log(choice)
+        }
+    }])
+    
 
+    .controller('ArticlesController', ['openSearch', function(openSearch){
+        var self = this;
+        this.isEmpty = true;
+        this.testing = function(input) {
+            console.log(1, input)
+        }
+        this.getArticles = function(input, limit) {
+            openSearch.getSuggestions(input, limit, function(res) {
+                self.titles = res.data[1];
+                self.descriptions = res.data[2];
+                self.links = res.data[3];
+                self.isEmpty = false;
+            })
+        }
     }])
--- js/services.js
+++ js/services.js
@@ -1,6 +1,6 @@
 angular.module('wikiReader')
     .service('openSearch', ['$http', function($http) {
-        this.suggest = function(input, limit, callback){
+        this.getSuggestions = function(input, limit, callback){
             if (input) {
                 $http.jsonp(`http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${input}&limit=${limit}&namespace=0&suggest=1&warningsaserror=1&callback=JSON_CALLBACK`).then(callback)
             }
